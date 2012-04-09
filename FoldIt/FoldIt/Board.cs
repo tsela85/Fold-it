cce5225c61afedd1665fce49d019b23d635cb77e
Tom Sela <tsela85@gmail.com>
2012-04-09 14:15:37 +0300

    adding project files

---
+++ FoldIt/FoldIt/Board.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace FoldIt
+{
+   public enum Edge {Top,Bottom,Left,Right,None};
+
+    public struct EdgePosition
+    {
+        public Edge theEdge;
+        public int x,y;
+    }
+
+    class Board
+    {
+        public const int DISTANCEfromSCREEN = 50;
+        public const int DISTANCEfromOUTER = 30;
+
+        Texture2D outerTex;
+        Texture2D innerTex;
+        int outX, outY, outH, outW;
+        int inX, inY, inX1, inY1;
+        EdgePosition edge1, edge2;
+        MouseState ms;
+
+        public Board(Texture2D outT,Texture2D inT,int screenW,int screenH)
+        {
+            outX = outY = DISTANCEfromSCREEN;
+            outH = screenH - 2*DISTANCEfromSCREEN;
+            outW = screenW - 2*DISTANCEfromSCREEN;
+
+            inX = inY = DISTANCEfromSCREEN+DISTANCEfromOUTER;
+            inY1 = screenH - DISTANCEfromSCREEN - DISTANCEfromOUTER;
+            inX1 = screenW - DISTANCEfromSCREEN - DISTANCEfromOUTER;
+
+            outerTex = outT;
+            innerTex = inT;
+
+            edge1 = new EdgePosition();
+            edge2 = new EdgePosition();
+
+            
+        }
+
+        public GameState Update(GameState gamestate, GameTime gameTime) 
+        {
+            Edge currentEdge = onEdge();
+            ms = Mouse.GetState();
+
+            if (gamestate == GameState.chooseEdge1)
+            {
+                if (currentEdge != Edge.None)
+                {
+                    edge1.theEdge = currentEdge;
+                    switch (currentEdge)
+                    {
+                        case (Edge.Top): edge1.x = ms.X; edge1.y = inY-3; break;
+                        case (Edge.Bottom): edge1.x = ms.X; edge1.y = inY1-6; break;
+                        case (Edge.Left): edge1.x = inX-3; edge1.y = ms.Y; break;
+                        case (Edge.Right): edge1.x = inX1-6; edge1.y = ms.Y; break;
+                        default: edge1.x = ms.X; edge1.y = ms.Y; break;
+                    }
+                    return GameState.onEdge1; //((ms.LeftButton == ButtonState.Pressed) ? GameState.onEdge1 : GameState.chooseEdge1);
+                }
+            } 
+            return GameState.chooseEdge1;
+        }
+
+        public void Draw(SpriteBatch spriteBatch,GameState gamestate)
+        {
+            spriteBatch.Draw(outerTex, new Rectangle(outX, outY, outW, outH), Color.RosyBrown);
+            spriteBatch.Draw(innerTex, new Rectangle(inX, inY, inX1 - inX, inY1 - inY), Color.Salmon);
+            if (gamestate == GameState.onEdge1)
+                spriteBatch.Draw(outerTex, new Rectangle(edge1.x, edge1.y, 8, 8), Color.Yellow);
+
+        }
+
+        private Edge onEdge()
+        {
+            if ((ms.X >= inX) && (ms.X <= inX1) && (ms.Y <= inY +3) && (ms.Y >= inY -3))
+                return Edge.Top;
+            if ((ms.X >= inX) && (ms.X <= inX1) && (ms.Y <= inY1 +3) && (ms.Y >= inY1 -3))
+                return Edge.Bottom;
+            if ((ms.Y >= inY ) && (ms.Y <= inY1) && (ms.X <= inX +3) && (ms.X >= inX -3))
+                return Edge.Left;
+            if ((ms.Y >= inY) && (ms.Y <= inY1) && (ms.X <= inX1 +3) && (ms.X >= inX1 -3))
+                return Edge.Right;
+            return Edge.None;
+        }
+    }
+}
---
+++ FoldIt/FoldIt/Game1.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace FoldIt
+{
+    enum GameState {chooseEdge1,onEdge1, chooseEdge2,onEdge2, folding };
+   
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Board board;
+        GameState gamestate;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            #region screenInit
+            graphics.PreferredBackBufferWidth = 1200;
+            graphics.PreferredBackBufferHeight = 600;
+            //graphics.IsFullScreen = false;
+            graphics.ApplyChanges();
+           // Window.AllowUserResizing = true; 
+            Window.Title = "Fold It";
+            #endregion
+
+            gamestate = GameState.chooseEdge1;
+            this.IsMouseVisible = true;
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            //GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            //GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width
+            
+            board = new Board(Content.Load<Texture2D>(@"empty"),Content.Load<Texture2D>(@"edged"),
+                graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            gamestate = board.Update(gamestate,gameTime);
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            board.Draw(spriteBatch,gamestate);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
